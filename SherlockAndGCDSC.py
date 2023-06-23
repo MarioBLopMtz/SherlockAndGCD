de776a89e204d081f73952a3639e17c37b2c7f05
MarioBLopMtz <137469342+MarioBLopMtz@users.noreply.github.com>
2023-06-23 08:31:53 -0600

    Update SherlockAndGCDSC.py

--- SherlockAndGCDSC.py
+++ SherlockAndGCDSC.py
@@ -7,18 +7,15 @@ import re
 import sys
 
 def solve(a):
-    a.sort()
     
-    sum_digits = sum(int(digit) for digit in str(a[0]))
-  
-    if all(x == a[0] for x in a):
-        return "NO"  
-
+    n = len(a)
     for num in a:
-        if num % sum_digits == 0:
-            return "NO"  
-    
-    return "YES"  
+        if num == 1:
+            return "YES"
+        for i in range(2, int(num ** 0.5) + 1):
+            if num % i == 0:
+                return "NO"
+    return "YES"
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
