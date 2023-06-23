9c1758c077ac3b429f20f80cf0b10d74d1eb6ad0
MarioBLopMtz <137469342+MarioBLopMtz@users.noreply.github.com>
2023-06-23 08:33:24 -0600

    Update SherlockAndGCDCC.py

--- SherlockAndGCDCC.py
+++ SherlockAndGCDCC.py
@@ -7,35 +7,33 @@ import re
 import sys
 
 def solve(a):
-    # Ordena el arreglo en orden ascendente
-    a.sort()
-    
-    # Suma los dígitos del primer elemento del arreglo
-    sum_digits = sum(int(digit) for digit in str(a[0]))
-    
-    # Verifica si todos los elementos del arreglo son iguales
-    if all(x == a[0] for x in a):
-        return "NO"  # Si todos los elementos son iguales, no existe un subconjunto que cumpla las condiciones
-    
-    # Verifica si algún elemento del arreglo es divisible por la suma de dígitos del primer elemento
-    for num in a:
-        if num % sum_digits == 0:
-            return "NO"  # Si encuentra un elemento divisible, no existe un subconjunto que cumpla las condiciones
-    
-    return "YES"  # Si no se cumple ninguna de las condiciones anteriores, existe un subconjunto que cumple las condiciones
+    # Función para determinar si existe un subconjunto válido en el arreglo 'a'
+
+    n = len(a)  # Tamaño del arreglo 'a'
+
+    for num in a:  # Iterar sobre cada número en el arreglo 'a'
+        if num == 1:
+            return "YES"  # Si hay un 1 en el arreglo, automáticamente se considera válido
+
+        for i in range(2, int(num ** 0.5) + 1):
+            # Verificar si el número 'num' es divisible por algún número 'i' en el rango de 2 a la raíz cuadrada de 'num'
+            if num % i == 0:
+                return "NO"  # Si es divisible, el subconjunto no es válido y se devuelve "NO"
+
+    return "YES"  # Si no se encontraron divisores para ningún número, el subconjunto es válido y se devuelve "YES"
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
-    t = int(input().strip())  # Lee el número de casos de prueba
+    t = int(input().strip())  # Leer el número de casos de prueba
 
     for t_itr in range(t):
-        a_count = int(input().strip())  # Lee el tamaño del arreglo
+        a_count = int(input().strip())  # Leer el tamaño del arreglo 'a'
 
-        a = list(map(int, input().rstrip().split()))  # Lee los elementos del arreglo como una lista de enteros
+        a = list(map(int, input().rstrip().split()))  # Leer los elementos del arreglo 'a'
 
-        result = solve(a)  # Llama a la función solve para resolver el problema
+        result = solve(a)  # Llamar a la función solve para obtener el resultado
 
-        fptr.write(result + '\n')  # Escribe el resultado en el archivo de salida
+        fptr.write(result + '\n')  # Escribir el resultado en el archivo de salida
 
     fptr.close()
